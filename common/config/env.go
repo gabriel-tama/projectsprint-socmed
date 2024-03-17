fd9c9bfeb3443930ebf774c97cde16f55bdc34f8
gabriel-tama <gabrieltama777@gmail.com>
2024-03-18 00:17:51 +0700

    init debee

---
+++ common/config/env.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string
+	Host string
+
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBName     string
+	DBPassword string
+
+	S3Bucket string
+	S3Secret string
+	S3ID     string
+	S3Url    string
+	S3Region string
+
+	JWTSecret   string
+	BCRYPT_Salt int
+	JWTExp      int
+}
+
+func Get() (*Config, error) {
+
+	var Conf *Config
+	err := godotenv.Load()
+	if err != nil {
+        fmt.Println(err)
+		log.Fatal("Error loading .env file")
+	}
+
+	JWTExp, err := strconv.Atoi(os.Getenv("JWT_EXPIRATION"))
+	if err != nil {
+		log.Fatal("Error parsing JWT_EXPIRATION")
+		JWTExp = 60
+	}
+
+	salt, err := strconv.Atoi(os.Getenv("BCRYPT_SALT"))
+	if err != nil {
+		log.Fatal("Error parsing BCRYPT_SALT")
+		salt = 10
+	}
+
+	Conf = &Config{
+		Port: os.Getenv("PORT"),
+		Host: os.Getenv("HOST"),
+
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USERNAME"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+
+		S3Bucket: os.Getenv("S3_BUCKET_NAME"),
+		S3Secret: os.Getenv("S3_SECRET_KEY"),
+		S3ID:     os.Getenv("S3_ID"),
+		S3Url:    os.Getenv("S3_BASE_URL"),
+		S3Region: os.Getenv("S3_REGION"),
+
+		JWTSecret:   os.Getenv("JWT_SECRET"),
+		BCRYPT_Salt: salt,
+		JWTExp:      JWTExp,
+	}
+
+	return Conf, nil
+}
--- go.mod
+++ go.mod
@@ -2,12 +2,17 @@ module github.com/gabriel-tama/projectsprint-socmed
 
 go 1.22.1
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 )
 
