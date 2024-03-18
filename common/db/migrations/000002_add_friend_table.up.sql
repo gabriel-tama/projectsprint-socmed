0209f9d847f2031d589a0e103cc63e9d992ede5c
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-03-18 19:50:24 +0700

    Add friend table migration

---
+++ common/db/migrations/000002_add_friend_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS "freiends" (
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    friend_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, friend_id)
+);
